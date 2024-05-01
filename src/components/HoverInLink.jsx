d2fa69fcfd202a7865785608616ae9ce4d6aa7a0
Cyborgnetical <Cyborgnetical@outlook.com>
2024-04-30 20:23:32 -0700

    added fancy stuff

---
+++ src/components/HoverInLink.jsx
@@ -0,0 +1,18 @@
+
+import style from "./HoverinLink.less"
+import React from "react"
+
+function A(props){
+
+    // if href exsits, set to href. else, set to text
+    let redirect = props.href ? props.href : props.text
+
+    let aa = <a href={redirect} className=" text-cyan-500 font-medium glowOption">
+        {props.text}
+    </a>;
+    
+
+    return aa
+}
+
+export default A
