b81f751f6190c1abe7826f697d155eef66b76654
Cyborgnetical <Cyborgnetical@outlook.com>
2024-05-03 08:14:13 -0700

    fixed redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,10 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  redirects: {
+    '/Home': '/',
+ },
 });
+import { defineConfig } from 'astro/config';
+
