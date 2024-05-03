9beaa5c3ee7294491d1c6ff4e37d78d03d7ce1ae
Cyborgnetical <Cyborgnetical@outlook.com>
2024-05-03 08:01:40 -0700

    added cabinet

--- src/components/HoverInLink.jsx
+++ src/components/HoverInLink.jsx
@@ -6,11 +6,11 @@ function A(props){
 
     // if href exsits, set to href. else, set to text
     let redirect = props.href ? props.href : props.text
+    let timeout = 5
 
     let aa = <a href={redirect} className=" text-cyan-500 font-medium glowOption">
         {props.text}
     </a>;
-    
 
     return aa
 }
