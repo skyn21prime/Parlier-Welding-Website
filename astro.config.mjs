dd0430d0e40597d7a380bce9231e7d1f90e641ac
Cyborgnetical <Cyborgnetical@outlook.com>
2024-05-03 09:08:19 -0700

    tesitng

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,9 @@ export default defineConfig({
   redirects: {
     '/Home': '/',
  },
+ buildOptions: {
+  publicPath: '/Parlier-Welding-Website/',
+},
 });
 import { defineConfig } from 'astro/config';
 
